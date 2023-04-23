c2f09c6872916eb7c7c9b03e39faed43153d20bc
Aboubacar Diallo <a.diallo3012@gmail.com>
2023-04-23 16:27:37 +0200

    first commit

---
+++ categories.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const categories = [
+    {
+        "categorie": "Entrées et plats composés",
+        "id": "cat_2487"
+    },
+    {
+        "categorie": "Fruits, légumes, légumineuses et oléagineux",
+        "id": "cat_9412"
+    },
+    {
+        "categorie": "Produits céréaliers",
+        "id": "cat_6591"
+    },
+    {
+        "categorie": "Viandes, œufs, poissons et assimilés",
+        "id": "cat_3611"
+    },
+    {
+        "categorie": "Produits laitiers et assimilés",
+        "id": "cat_7003"
+    },
+    {
+        "categorie": "Eaux et autres boissons",
+        "id": "cat_6449"
+    },
+    {
+        "categorie": "Produits sucrés",
+        "id": "cat_5315"
+    },
+    {
+        "categorie": "Glaces et sorbets",
+        "id": "cat_2538"
+    },
+    {
+        "categorie": "Matières grasses",
+        "id": "cat_9076"
+    },
+    {
+        "categorie": "Aides culinaires et ingrédients divers",
+        "id": "cat_6210"
+    }
+]
